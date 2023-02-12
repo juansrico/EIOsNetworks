1bec6df72421107f1c27d323c69ce12aa37d97a4
juansrico <107045777+juansrico@users.noreply.github.com>
2023-02-12 14:20:29 +0100

    Created code file

---
+++ Code.py
@@ -0,0 +1,23 @@
+# Data cleaning
+import pandas as pd
+url = r"C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\Fulldata1.csv"
+data = pd.read_csv(url)
+Headers = ["Organisation", "members", "headquarters", "all", "timestamp"]
+data.columns = Headers
+
+# Extract aims from the column "all"
+import re
+def extract_aims(text):
+    match = re.search("Aims\n.*\.", text)
+    if match:
+        aims = match.group()
+        aims = re.sub("Aims\n", "", aims)
+        return aims
+    else:
+        return None
+
+data['aims'] = data['all'].apply(extract_aims)
+print(data["aims"])
+
+# Save to CSV
+data.to_csv("sample_df.csv", index=False)
