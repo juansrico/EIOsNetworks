8ec27820d20b8c4a6e54aa7e3981ec9a335fce94
juansrico <107045777+juansrico@users.noreply.github.com>
2023-04-05 19:54:23 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -64,7 +64,46 @@ cols = data.columns.tolist()
 cols = cols[:1] + [cols[5]] + cols[1:5] + cols[6:]
 data = data[cols]
 
-
 # Save to CSV
 data.to_csv("DataProcessing1", index=False)
+
+
+# Manual process: delete organizations whose aims dont fit or dont have membership data (All those deleted case were listed in the document "deleted organizations".#
+# An additional manual step was to clean the column memberships (I deleted words such as "individuals in xxx countries" and included all countries listed, independent of whether it said "partner organizations in xxx countries" or similar
+
+### Create matrix for the network analysis
+
+# Before that: delete double listings of the same country in a specific space: 
+
+
+# Assuming your DataFrame is called 'df' and 'members' is the column with countries separated by commas
+df['members'] = df['members'].apply(lambda x: ', '.join(sorted(set(x.split(', ')))))
+
+df.to_csv("DataProcessing6", index=False)
+
+
+
+##Create matrix
+
+import pandas as pd
+from itertools import combinations
+
+# Assuming your DataFrame is called 'df'
+countries = set(df['members'].str.split(', ').sum())
+matrix = pd.DataFrame(0, index=countries, columns=countries)
+
+# Count the occurrences of each pair of countries
+for row in df['members']:
+    for a, b in combinations(row.split(', '), 2):
+        matrix.loc[a, b] += 1
+        matrix.loc[b, a] += 1
+
+# Print the resulting matrix
+print(matrix)
+
+dd = pd.DataFrame(matrix)
+
+# save dataframe as a csv file
+dd.to_csv('mydata.csv', index=True)
+
 
