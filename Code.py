1226b738c93ddfb8cb2ecafd8059519c505e4748
juansrico <107045777+juansrico@users.noreply.github.com>
2023-04-29 13:15:23 +0200

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,13 +1,32 @@
-# Data cleaning
+"""
+#### §§ This code is the step-by-step documentation of how I prepared the data downloaded from the YIO and conducted network analysis out of it §§    ####
+STEPS:
+
+0. Extract data from the online repository of the YIO
+- Search criteria
+  -> Environment & conservation. Categories: type I-> A,B,C,D,F. Type II (exclude): C,D,E,J
+  -> Extraction led to 612 results
+  -> Date: 07.02.2023
+  
+1. Data cleaning
+2. Manual selection of organizations that do work in the environmental field
+3. Transform dataset to a weighted matrix (edgelist) where the nodes are the countries and the ties are the number of shared memberships in EINGOs
+4. Create network graphic and conduct analyses
+
+"""
+########### 2- Data cleaning ###############
+# Load packages
 import pandas as pd
-url = r"C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\Original Data.csv"
+import re
+
+
+# Load data 
+url = r"C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\Original Data.csv"  
 data = pd.read_csv(url)
 Headers = ["Organisation", "members", "headquarters", "all", "timestamp"]
 data.columns = Headers
 
-
-# Extract aims from the column "all"
-import re
+# Extract aims from the column "all" and create the column "aims" of the organizations
 def extract_aims(text):
     match = re.search("Aims\n.*\.", text)
     if match:
@@ -20,18 +39,12 @@ def extract_aims(text):
 data['aims'] = data['all'].apply(extract_aims)
 print(data["aims"])
 
-# Eliminate empty spaces in the column "organisation"
+# Eliminate empty spaces in the column "organisation" 
+remove_extra_spaces = lambda x: ' '.join(x.split()) # define lambda function to eliminate extra spaces
+data['Organisation'] = data['Organisation'].apply(remove_extra_spaces) # apply lambda function to column
+print(data["Organisation"]) # See results
 
-# define lambda function to eliminate extra spaces
-remove_extra_spaces = lambda x: ' '.join(x.split())
-
-# apply lambda function to column
-data['Organisation'] = data['Organisation'].apply(remove_extra_spaces)
-# See results
-print(data["Organisation"])
-
-# Extract member countries from the column "all"
-import re
+# Extract member countries from the column "all" and create the column "members"
 def extract_members(text):
     match = re.search("Member Countries.*?(?=Type)", text, re.DOTALL)
     if match:
@@ -42,7 +55,7 @@ def extract_members(text):
     else:
         return None
 
-data['members'] = data['all'].apply(extract_members)
+data['members'] = data['all'].apply(extract_members) 
 print(data["members"])
 
 # Clean the new column "members" by replacing useless words with empty spaces
@@ -67,28 +80,30 @@ data = data[cols]
 # Save to CSV
 data.to_csv("DataProcessing1", index=False)
 
+########### 3- Manual selection ###############
+"""
+1 - Delete organizations whose aims are not related to environmental protection or fight against climate change, or that do not have membership data: 
+All those deleted case were listed in the document "deleted organizations".
+(QUESTION: What is the sheet "orgas with unfitting goals" (col) doing?
 
-# Manual process: delete organizations whose aims dont fit or dont have membership data (All those deleted case were listed in the document "deleted organizations".#
-# An additional manual step was to clean the column memberships (I deleted words such as "individuals in xxx countries" and included all countries listed, independent of whether it said "partner organizations in xxx countries" or similar
+2- Clean the column memberships: 
+Deleted words such as "individuals in xxx countries" and included all countries listed, independent of whether it said "partner organizations in xxx countries" 
+or something similar
 
-### Create matrix for the network analysis
+
+"""
+
+######## 4 - Create matrix for the network analysis  ###################
 
 # Before that: delete double listings of the same country in a specific space: 
-# Define a function to remove repeated words within a string
-def remove_duplicates(row):
+def remove_duplicates(row): # Define a function to remove repeated words within a string
     words = row.split(', ')
     return ', '.join(sorted(set(words), key=words.index))
 
-# Apply the function to the members column
-df['members'] = df['members'].apply(remove_duplicates)
-
-# Display the resulting dataframe
+df['members'] = df['members'].apply(remove_duplicates) # Apply the function to the members column
 print(df["members"])
 
-
-##Create matrix
-
-import pandas as pd
+#Create matrix
 from itertools import combinations
 
 # Assuming your DataFrame is called 'df'
@@ -108,5 +123,32 @@ dd = pd.DataFrame(matrix)
 
 # save dataframe as a csv file
 dd.to_csv('mydata.csv', index=True)
+
+
+######## START THE ANALYSIS ############## 
+# This procedure takes the file "mydata.csv" (edgelist format) and makes a network out of it.
+
+# Load the data
+with open(r'C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\mydata.csv', 'r') as f:
+    reader = csv.reader(f)
+    node_names = next(reader)[1:]  # extract the node names from the first row
+    weighted_matrix = np.array([[float(x) for x in row[1:]] for row in reader])
+
+# Create the graph (network)
+G = nx.Graph()
+
+# Add nodes to the graph
+num_nodes = len(node_names)
+G.add_nodes_from(node_names)
+
+# Add weighted edges to the graph
+for i in range(num_nodes):
+    for j in range(num_nodes):
+        weight = weighted_matrix[i][j]
+        if weight != 0:
+            G.add_edge(node_names[i], node_names[j], weight=weight)
+
+print(nx.info(G))  # Get basic info about the network
+
 
 
