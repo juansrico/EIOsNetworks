4f932e2cdfe0c83cc610e46cbeb4e1ad267ad671
juansrico <107045777+juansrico@users.noreply.github.com>
2023-11-01 17:31:38 -0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -11,7 +11,8 @@ STEPS:
 1. Data cleaning
 2. Manual selection of organizations that do work in the environmental field
 3. Transform dataset to a weighted matrix (edgelist) where the nodes are the countries and the ties are the number of shared memberships in EINGOs
-4. Create network graphic and conduct analyses
+4. Create metrics & conduct analyses
+5. Create graph
 
 """
 ########### 2- Data cleaning ###############
@@ -156,22 +157,103 @@ for i in range(num_nodes):
         if weight != 0:
             G.add_edge(node_names[i], node_names[j], weight=weight)
 
+# Print network metrics with explanations
+print("Network metrics:")
+print("Number of nodes:", num_nodes, "(The total number of nodes in the network)")
+print("Number of edges:", num_edges, "(The total number of edges in the network)")
+print("Network density:", density, "(A measure of how dense the network is, ranging from 0 to 1)")
+print("Average clustering coefficient:", avg_clustering_coef, "(The average local clustering coefficient of the nodes)")
+print("Average shortest path length:", avg_shortest_path_len, "(The average shortest path length between all pairs of nodes)")
 
-            
-            
- 
+
+## Calculate individual centrality measures
+import csv
+import networkx as nx
+
+# Calculate centrality measures
+degree_centralities = nx.degree_centrality(G)  # Fraction of nodes the node is connected to (how popular a country is)
+closeness_centralities = nx.closeness_centrality(G)  # How close a country is to all other countries in the network (how quickly information spreads)
+betweenness_centralities = nx.betweenness_centrality(G)  # How often a country acts as a bridge along the shortest path between two other countries (how influential a country is in connecting others)
+eigenvector_centralities = nx.eigenvector_centrality(G)  # A measure of the influence of a country in the network (how well-connected a country is to other well-connected countries)
+clustering_coefficients = nx.clustering(G)  # How connected a country's neighbors are to each other (how tightly knit a country's connections are)
+
+# Prepare data for the CSV file
+rows = []
+for node in G.nodes():
+    row = [
+        node,
+        degree_centralities[node],
+        closeness_centralities[node],
+        betweenness_centralities[node],
+        eigenvector_centralities[node],
+        clustering_coefficients[node]
+    ]
+    rows.append(row)
+
+# Define the CSV file path
+csv_file_path = r'C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\FINAL DATASETS\metrics Final.csv'
+
+# Write data to the CSV file
+with open(csv_file_path, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    headers = ["Node", "Degree Centrality (Popularity)", "Closeness Centrality (Closeness to Others)",
+               "Betweenness Centrality (Influence in Connecting Others)",
+               "Eigenvector Centrality (Influence in the Network)",
+               "Clustering Coefficient (Tightness of Connections)"]
+    writer.writerow(headers)
+    writer.writerows(rows)
+
+print("CSV file exported successfully!")
+
+
+# (Optional) # Create correlation matrix and heatmap for facilitating the interpretation 
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Replace the file path with the actual path to your CSV file
+file_path = r"C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\FINAL DATASETS\Matrix network Final.csv"
+
+# Read the CSV file into a pandas DataFrame
+df = pd.read_csv(file_path, index_col=0)
+
+# Calculate correlation matrix to identify patterns in shared memberships
+correlation_matrix = df.corr()
+
+# Export the correlation matrix to a CSV file
+correlation_matrix.to_csv(r"C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\FINAL DATASETS\CorrelationMatrix.csv")
+
+# Visualize the correlation matrix using a heatmap with clear titles
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title("Correlation Matrix of Shared Memberships")
+plt.xlabel("Countries")
+plt.ylabel("Countries")
+plt.savefig(r"C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\FINAL DATASETS\CorrelationMatrixHeatmap.png")
+plt.show()
+
+print("Correlation Matrix exported to CorrelationMatrix.csv")
+print("Heatmap saved as CorrelationMatrixHeatmap.png")
+
+
+###### 5- Create Graphic #########
+
+### printing the graph
+
 import csv
 import numpy as np
 import networkx as nx
-
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 # Load the data
-with open(r'C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\mydata.csv', 'r') as f:
+with open(r'C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\mydata4.csv', 'r') as f:
     reader = csv.reader(f)
     node_names = next(reader)[1:]  # extract the node names from the first row
     weighted_matrix = np.array([[float(x) for x in row[1:]] for row in reader])
 
-# Create the graph
+# Create the graph (network)
 G = nx.Graph()
 
 # Add nodes to the graph
@@ -185,27 +267,31 @@ for i in range(num_nodes):
         if weight != 0:
             G.add_edge(node_names[i], node_names[j], weight=weight)
 
-            
-### PRINT NETWORKS' METRICS            
-print(nx.info(G))  # Get basic info about the network
+# Compute node centrality
+centrality = nx.degree_centrality(G)
 
-density = nx.density(G)
-print("Network density:", density)
+# Filter extreme outliers (optional)
+threshold = 0.1  # Adjust the threshold as needed
+filtered_nodes = [node for node, centrality_value in centrality.items() if centrality_value > threshold]
+G_filtered = G.subgraph(filtered_nodes)
 
-num_nodes = nx.number_of_nodes(G)
-num_edges = nx.number_of_edges(G)
-density = nx.density(G)
-avg_clustering_coef = nx.average_clustering(G)
-avg_shortest_path_len = nx.average_shortest_path_length(G)
-degree_centralities = nx.degree_centrality(G)
-closeness_centralities = nx.closeness_centrality(G)
-betweenness_centralities = nx.betweenness_centrality(G)
+# Compute node sizes based on centrality
+node_sizes = [centrality[node] * 2000 for node in G_filtered.nodes()]
 
-print("Number of nodes:", num_nodes)
-print("Number of edges:", num_edges)
-print("Density:", density)
-print("Average clustering coefficient:", avg_clustering_coef)
-print("Average shortest path length:", avg_shortest_path_len)
-print("Degree centralities:", degree_centralities)
-print("Closeness centralities:", closeness_centralities)
-print("Betweenness centralities:", betweenness_centralities)
+# Compute node colors based on centrality
+centrality_values = [centrality[node] for node in G_filtered.nodes()]
+min_centrality = min(centrality_values)
+max_centrality = max(centrality_values)
+norm_centrality_values = [(np.log10(x) - np.log10(min_centrality)) / (np.log10(max_centrality) - np.log10(min_centrality)) for x in centrality_values]
+color_map = cm.ScalarMappable(cmap=cm.viridis)
+color_map.set_array(centrality_values)
+node_colors = color_map.to_rgba(norm_centrality_values)
+
+# Visualize the network
+pos = nx.spring_layout(G_filtered, seed=42)  # layout algorithm for node positioning
+plt.figure(figsize=(40, 40))  # adjust the figure size
+nx.draw_networkx(G_filtered, pos=pos, with_labels=True, node_color=node_colors, edge_color='gray', alpha=0.8,
+                 node_size=node_sizes, font_size=8, font_color='black', font_weight='bold')
+
+# Save the network visualization to a file
+plt.savefig(r'C:\Users\ricoe\OneDrive\Documents\WZB\PAPER\network_visualization2.jpg')
